2b3698e03c4ef36bac5168c49eb11f8ee70105c3
Uday Korlimarla <44824134+udaykor@users.noreply.github.com>
2023-08-23 14:28:53 +1000

    use ID from URL directly for tainted flow

--- app/api/fleet/state/[id]/route.ts
+++ app/api/fleet/state/[id]/route.ts
@@ -2,9 +2,8 @@ import { getVehicle } from "@tesla-web/lib/state"
 import { NextResponse } from "next/server"
 
 // id and vehicle_id can be numbers?
-export async function GET(request) {
+export async function GET(request:Request) {
   // const vehicle = await getVehicle(params.id)
-  const id = request.url.split('=')[1];
-  const vehicle = await getVehicle(id);
+  const vehicle = await getVehicle(request.url.split('=')[1]);
   return NextResponse.json({ vehicle })
 }
