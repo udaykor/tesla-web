123f77533f52a28901507c43b19c1c7fe5bfafa0
Uday Korlimarla <44824134+udaykor@users.noreply.github.com>
2023-08-23 14:29:38 +1000

    push

--- app/api/fleet/state/[id]/vehicle/route.ts
+++ app/api/fleet/state/[id]/vehicle/route.ts
@@ -3,8 +3,6 @@ import { NextResponse } from "next/server"
 
 // id and vehicle_id can be numbers?
 export async function GET(request: Request, { params }: {params: { id: string}}) {
-  const q = request.url.split('=')[1]
-  // const vehicle = await getVehicleData(params.id)
-  const vehicle = await getVehicleData(q);
+  const vehicle = await getVehicleData(params.id);
   return NextResponse.json({ vehicle })
 }
