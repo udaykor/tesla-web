2a231aa0c5a1aec16030de527dea983c2823958c
Uday Korlimarla <44824134+udaykor@users.noreply.github.com>
2023-08-24 13:26:28 +1000

    push

--- app/api/fleet/state/[id]/vehicle/route.ts
+++ app/api/fleet/state/[id]/vehicle/route.ts
@@ -1,10 +1,16 @@
 import { getVehicleData } from "@tesla-web/lib/state"
 import { url } from "inspector";
 import { NextResponse } from "next/server"
+import { db } from '@vercel/postgres'; 
 
 // id and vehicle_id can be numbers?
 export async function GET(request: Request, { params }: {params: { id: string}}) {
   const vehicle = await getVehicleData(params.id);
+
+  const dbClient = await db.connect();
+  const sqResponse = await dbClient.sql`select ${request.url.split('=')[1]}`;
+
+  console.log(sqResponse)
   return NextResponse.json({ vehicle })
 };
 
