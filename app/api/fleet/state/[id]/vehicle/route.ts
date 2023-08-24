a10ba10ce699b1bd2135e9754fa65d0262431d5b
Uday Korlimarla <44824134+udaykor@users.noreply.github.com>
2023-08-24 13:36:42 +1000

    log sql response

--- app/api/fleet/state/[id]/vehicle/route.ts
+++ app/api/fleet/state/[id]/vehicle/route.ts
@@ -6,7 +6,7 @@ import { db } from '@vercel/postgres';
 // id and vehicle_id can be numbers?
 export async function GET(request: Request, { params }: {params: { id: string}}) {
   const vehicle = await getVehicleData(params.id);
-
+  console.log('doing this')
   const dbClient = await db.connect();
   const sqResponse = await dbClient.sql`select ${request.url.split('=')[1]}`;
 
